1c762515f66261f4bd80dc6396f0188c859c55b1
at54030230 <e.ek.84147098480789650665@gmail.com>
2017-02-17 22:12:10 +0900

    [Modify]sort

--- MD-GPU-Grid/sort.cu
+++ MD-GPU-Grid/sort.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 #include <time.h>
+#include "common/common_vc.h"
 
 
 ///
@@ -23,13 +24,31 @@ __global__ void bitonicSort_Global(int *dep,int *tar,int i,int j){
     tar[idx] = ins?sel:opp;
 }
 
-__global__ void bitonicSort_Local1(int *dep,int *tar,int i,int j){
+
+// 2^j <= blockDim  & blockDim = 2^k
+__global__ void bitonicSort_Local(int *dep,int *tar,int i,int jMax){
     extern __shared__ int sbuf [];
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     int itx = threadIdx.x;
     
     sbuf[itx] = dep[idx];
     __syncthreads();
+    
+    
+    for (int j=jMax;j>=0;j--){
+        int dec = (idx >> (i+1))&0x1;
+        int jtx = itx ^ (1<<j);
+        int rig = (itx >> j)&0x1;
+        int sel = sbuf[itx];
+        int opp = sbuf[jtx];
+        bool ima = opp < sel;
+        bool ins = (dec ^ rig) == ima;
+        __syncthreads();
+        sbuf[itx] = ins?sel:opp;
+        __syncthreads();
+    }
+
+    tar[idx] = sbuf[itx];
 }
 
 void initRand(){
@@ -39,7 +58,7 @@ void initRand(){
 void initializeBuffer(int *buf,int nElem){
     for (int i=0;i<nElem;i++){
         buf[i]=(int)( ((double)rand()/(RAND_MAX+1))*1000 );
-        printf("%03d,",buf[i]);
+        //printf("%03d,",buf[i]);
     }
     printf("\n");
 }
@@ -55,9 +74,10 @@ int main(int argc,char** argv){
 
     initRand();
     
-    int nLogE = 8;
+    int nLogE = 17;
     int nElem = 1<<nLogE;
-    int nBytes = nElem * sizeof(int);
+    size_t nBytes = nElem * sizeof(int);
+    printf("nElem=%d\n",nBytes);
     
     //CPU BUF
     int *h_buf;
@@ -65,30 +85,42 @@ int main(int argc,char** argv){
     
     //GPU BUF
     int *d_buf,*d_bufs;
-    cudaMalloc(&d_buf ,nBytes);
-    cudaMalloc(&d_bufs,nBytes);
+    CHECK(cudaMalloc(&d_buf ,nBytes));
+    CHECK(cudaMalloc(&d_bufs,nBytes));
     
     //DATA INIT
     
     initializeBuffer(h_buf,nElem);
     
     //START
-    cudaMemcpy(d_buf,h_buf,nBytes,cudaMemcpyHostToDevice);
+    CHECK(cudaMemcpy(d_buf,h_buf,nBytes,cudaMemcpyHostToDevice));
     
     for(int i=0;i<nLogE;i++){
-        for(int j=i;j>=0;j--){
-                bitonicSort_Global<<<nElem/32,32>>>(d_buf,d_bufs,i,j);
+        int j = i;
+        int nLogL = 8;
+        int nSizL = 1<<nLogL;
+        for(;j>= nLogL;j--){
+                bitonicSort_Global<<<nElem/256,256>>>(d_buf,d_bufs,i,j);
                 SwapIntPointer(&d_buf,&d_bufs);
                 cudaDeviceSynchronize();
         }
+        
+        CHECK(cudaGetLastError());
+        
+        bitonicSort_Local<<<(nElem+nSizL-1)/nSizL,nSizL,nSizL*sizeof(int)>>>(d_buf,d_bufs,i,j);
+        SwapIntPointer(&d_buf,&d_bufs);
+        cudaDeviceSynchronize();
+        
+        //printf("%d\n",j);
+        CHECK(cudaGetLastError());
     }
 
-
+    
     
     cudaMemcpy(h_buf,d_buf,nBytes,cudaMemcpyDeviceToHost);
-    for (int i=0;i<nElem;i++){
-        printf("%03d,",h_buf[i]);
-    }printf("\n");
+    //for (int i=0;i<nElem;i++){
+    //    printf("%03d,",h_buf[i]);
+    //}printf("\n");
     
     
 
