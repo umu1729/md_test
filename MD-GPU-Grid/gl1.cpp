5931e1296c29a9414ae3cdcbaa5a459231ba1b67
at54030230 <e.ek.84147098480789650665@gmail.com>
2017-02-17 20:13:17 +0900

    [ADD]naive-sort

---
+++ MD-GPU-Grid/gl1.cpp
@@ -0,0 +1,101 @@
+//#pragma comment(linker, "/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup")
+#include "GL/freeglut.h"
+#include <string.h>
+#include "cuHeader.h"
+#include <stdio.h>
+
+#define WIDTH (640*2)
+#define HEIGHT (360*2)
+
+int Width;
+int Height;
+
+//平行移動用
+float x = 0.0f;
+bool flag = false;
+//緑
+GLfloat green[] = { 0.0, 1.0, 0.0, 1.0 };
+//ライトの位置
+GLfloat lightpos[] = { 200.0, 150.0, -500.0, 1.0 };
+
+
+V3Buf v3Buf;
+
+void display(void)
+{
+
+ glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+ glViewport(0, 0, Width,Height);
+ glMatrixMode(GL_PROJECTION);
+ glLoadIdentity();
+ //視野角,アスペクト比(ウィンドウの幅/高さ),描画する範囲(最も近い距離,最も遠い距離)
+ gluPerspective(90.0, (double)Width / (double)Height, 1.0, 1000.0);
+ glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+ //視点の設定
+ gluLookAt(6.0,6.0,-20.0, //カメラの座標
+      6.0,6.0,0.0, // 注視点の座標
+     0.0,1.0,0.0); // 画面の上方向を指すベクトル
+ //ライトの設定
+ glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
+ //マテリアルの設定
+ glMaterialfv(GL_FRONT, GL_DIFFUSE, green);
+ //平行移動
+    for (int i=0;i<v3Buf.nElem;i++){
+        glPushMatrix();
+        glTranslatef((v3Buf.xBuf)[i],(v3Buf.yBuf)[i],(v3Buf.zBuf)[i]);
+        glutSolidSphere(0.3,12,12);
+        glPopMatrix();
+    }
+ glutSwapBuffers();
+}
+void idle(void)
+{
+ if(flag){x-=0.5f;}else{x+=0.5f;}
+ if(x>50.0f)flag=true;
+ if(x<-50.0f)flag=false;
+ Sleep(1);
+ glutPostRedisplay();
+}
+void Init(){
+ glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+ glEnable(GL_DEPTH_TEST);
+ glEnable(GL_LIGHTING);
+ glEnable(GL_LIGHT0);
+
+}
+void NOTHING(){
+}
+void Loop(V3Buf buf){
+    v3Buf = buf;
+    
+    glutMainLoopEvent();
+    display();
+    idle();
+}
+void LoopNoDebug(V3Buf buf){
+}
+void Resize( int w, int h )
+{
+	Width = w;
+    Height = h;
+}
+int main(int argc, char **argv)
+{
+    //strcmp(argv[1],"debug"))
+    if (1==1){
+        glutInitWindowPosition(100, 100);
+        glutInitWindowSize(WIDTH, HEIGHT);
+        glutInit(&argc, argv);
+        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+        glutCreateWindow("球を表示して平行移動");
+        glutDisplayFunc(NOTHING);
+        glutIdleFunc(NOTHING);
+        glutReshapeFunc( Resize );
+        Init();
+        cuMain(Loop);
+    }else{
+        cuMain(LoopNoDebug);
+    }
+     return 0;
+}
---
+++ MD-GPU-Grid/sort.cu
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <time.h>
+
+
+///
+/// This is Naive-Bitonic-Sort;
+///
+
+__global__ void bitonicSort_Global(int *dep,int *tar,int i,int j){
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    int dec = (idx >> (i+1))&0x1; //ascend or decend flag;
+    int jdx = idx ^ (1<<j); //target buf
+    int rig = (idx >> j)&0x1; //left or right (buffer pointer) flag;
+    int sel = dep[idx];
+    int opp = dep[jdx];
+    int mi = min(sel,opp);
+    int ma = max(sel,opp);
+    tar[idx] = ((dec ^ rig) == 1)?ma:mi;  
+}
+
+__global__ void bitonicSort_Local1(){
+    extern __shared__ int sbuf [];
+    //int idx = threadIdx.x + 
+}
+
+void initRand(){
+    srand((unsigned) time(nullptr));
+}
+
+void initializeBuffer(int *buf,int nElem){
+    for (int i=0;i<nElem;i++){
+        buf[i]=(int)( ((double)rand()/(RAND_MAX+1))*1000 );
+        printf("%03d,",buf[i]);
+    }
+    printf("\n");
+}
+
+void SwapIntPointer(int** a,int **b){
+    int *tmp = *b;
+    *b = *a;
+    *a = tmp;
+}
+
+
+int main(int argc,char** argv){
+
+    initRand();
+    
+    int nLogE = 8;
+    int nElem = 1<<nLogE;
+    int nBytes = nElem * sizeof(int);
+    
+    //CPU BUF
+    int *h_buf;
+    h_buf = (int*)malloc(nBytes);
+    
+    //GPU BUF
+    int *d_buf,*d_bufs;
+    cudaMalloc(&d_buf ,nBytes);
+    cudaMalloc(&d_bufs,nBytes);
+    
+    //DATA INIT
+    
+    initializeBuffer(h_buf,nElem);
+    
+    //START
+    cudaMemcpy(d_buf,h_buf,nBytes,cudaMemcpyHostToDevice);
+    
+    for(int i=0;i<nLogE;i++){
+        for(int j=i;j>=0;j--){
+                bitonicSort_Global<<<nElem/32,32>>>(d_buf,d_bufs,i,j);
+                SwapIntPointer(&d_buf,&d_bufs);
+                cudaDeviceSynchronize();
+        }
+    }
+
+
+    
+    cudaMemcpy(h_buf,d_buf,nBytes,cudaMemcpyDeviceToHost);
+    for (int i=0;i<nElem;i++){
+        printf("%03d,",h_buf[i]);
+    }printf("\n");
+    
+    
+
+
+    return 0;
+}
